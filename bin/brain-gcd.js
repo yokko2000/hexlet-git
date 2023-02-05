7a1602152521cf81a453b78810dca07af8a1d668
Maksim Komiachko <yokko1990@yandex.ru>
2023-02-05 14:59:36 +0300

    add new file brain-progression

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -6,29 +6,34 @@ import readlineSync from 'readline-sync';
 const getRandomInt = () => {
     return Number(Math.floor(Math.random() * 101));
 }
-//функция генерирующая случайный знак операции
-const getRandomOperation = () => {
-    const array = ['+', '-', '*'];
-    return array[Math.floor(Math.random() * 2)];
+//функция поиска НОД
+function gcd_rec(a, b) {
+    if (b) {
+        return gcd_rec(b, a % b);
+    } else {
+        return Math.abs(a);
+    }
 }
 //спрашиваем как зовут пользователя
 var userName = readlineSync.question('May I have your name? ');
 //здесь идет основная логика игры
-console.log('What is the result of the expression?');
-let count = 0;
+console.log('Find the greatest common divisor of given numbers.');
+let point = 0;
 //цикл на 3 раунда игры
 for (let i = 0; i < 3; i++) {
     //здесь генерируем вопрос пользователю
-    var question = getRandomInt() + getRandomOperation() + getRandomInt();
+    var numberOne = getRandomInt();
+    var numberTwo = getRandomInt()
+    var question = numberOne + ' ' + numberTwo;
     //здесь генерируем правильный ответ
-    var correctQuestion = eval(question);
+    var correctQuestion = gcd_rec(numberOne, numberTwo);
     //здесь вывод вопроса и ввод ответа
     console.log('Question: ' + question);
     var answer = readlineSync.question('Your answer: ');
     //условие на проверку правильности ответа
     if (answer == correctQuestion) {
         console.log('Correct!');
-        count += 1;
+        point += 1;
     } else {
         console.log(answer + ' is wrong answer ;(. Correct answer was ' + correctQuestion + '.');
         console.log('Let\'\s try again, ' + userName)
@@ -37,7 +42,7 @@ for (let i = 0; i < 3; i++) {
 }
 //конец цикла
 //проверка что отвечено на 3 вопроса правильно и вывод поздравления
-if (count == 3) {
+if (point == 3) {
     console.log('Congratulations, ' + userName + '!')
 }
 //конец
---
+++ bin/brain-progression.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+ //начало
+//импорт нужных функций
+import readlineSync from 'readline-sync';
+//функция генерирующая случайное число от 0 до 100
+const getRandomInt = () => {
+    return Number(Math.floor(Math.random() * 101));
+}
+//спрашиваем как зовут пользователя
+var userName = readlineSync.question('May I have your name? ');
+//здесь идет основная логика игры
+console.log('What number is missing in the progression?');
+let point = 0;
+//цикл на 3 раунда игры
+for (let i = 0; i < 3; i++) {
+    //генерируем длину прогрессии в промежутке от 5 до 10
+    var lenghtOfProgression = Math.floor(Math.random() * (11 - 5) + 5);
+    var progression = [];
+    //генерируем шаг прогрессии
+    var stepOfProgression = getRandomInt();
+    //генерируем начальную точку прогрессии
+    var startPointOfProgression = getRandomInt();
+    //цикл генерирующий прогрессию
+    for (let i = 0; i < lenghtOfProgression; i += 1) {
+        progression.push(startPointOfProgression);
+        startPointOfProgression += stepOfProgression;
+    }
+    //делаем замену на точки для случайного элемента прогрессии
+    var skippedNumber = Math.floor(Math.random() * lenghtOfProgression);
+    var correctQuestion = progression[skippedNumber];
+    progression[skippedNumber] = '...';
+    //здесь вывод вопроса и ввод ответа
+    console.log('Question: ' + progression.join(" "));
+    var answer = readlineSync.question('Your answer: ');
+    //условие на проверку правильности ответа
+    if (answer == correctQuestion) {
+        console.log('Correct!');
+        point += 1;
+    } else {
+        console.log(answer + ' is wrong answer ;(. Correct answer was ' + correctQuestion + '.');
+        console.log('Let\'\s try again, ' + userName)
+        break;
+    }
+}
+//конец цикла
+//проверка что отвечено на 3 вопроса правильно и вывод поздравления
+if (point == 3) {
+    console.log('Congratulations, ' + userName + '!')
+}
+//конец
